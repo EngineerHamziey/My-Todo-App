0ad52e1817b3b8e3103fa17b6cbae08f989664b5
Hamziey <engineerlawalhamzatademola@gmail.com>
2022-08-04 17:24:37 +0100

    -v1 done

--- others/app.js
+++ others/app.js
@@ -1,5 +1,4 @@
 // localStorage.clear();
-
 // My selectors / Guys from the DOM
 const inputBox = document.querySelector(".input-box");
 const todoList = document.querySelector(".todo-list");
@@ -8,34 +7,54 @@ const saveButton = document.querySelector(".save-button");
 
 
 /*****************Event Listeners*******************************/
-// document.addEventListener("DOMContentLoaded", loadLocalTodos());
+document.addEventListener("DOMContentLoaded", FETCHLocalTodos());
 saveButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", saveOrDelete);
+filter.addEventListener("click", filterfunc);
 
 
 
 /*****************Functions************************/
-function addTodo(myeventi) {
-  // this EVENTI should prevent the form from submitting
-  myeventi.preventDefault();
+function FETCHLocalTodos() {
+  todoItems = JSON.parse(localStorage.getItem("todoItems"));
+  if (!(todoItems === null)) {
+    todoItems.forEach(function(item){
+      showTodoInDom(item);
+  });
+
+  }
+}
+
+function showTodoInDom(eachTodo) {
   // todoItems is the list item itself
   const newTodoItem = document.createElement("li");
-  newTodoItem.classList.add("todo-items")
+  newTodoItem.classList.add("todo-items");
   // todoText is the div containing the text
   const todoText = document.createElement("div");
   todoText.classList.add("todo-text");
   // adding the innertext of the input element to it
-  todoText.innerText = inputBox.value;
+  todoText.innerText = eachTodo;
   newTodoItem.appendChild(todoText);
   // save button
   const doneBtn = document.createElement("button");
-  doneBtn.classList.add("done-btn")
+  //Add icon to button
+  doneBtn.innerHTML = '<i class="fas fa-check ico" aria-hidden="true"></i>';
+  doneBtn.classList.add("done-btn");
   newTodoItem.appendChild(doneBtn);
   const deleteBtn = document.createElement("button");
+  //Add icon to button
+  deleteBtn.innerHTML = '<i class="fas fa-trash ico" aria-hidden="true"></i>';
   deleteBtn.classList.add("delete-btn");
   newTodoItem.appendChild(deleteBtn);
   // now add the new todo to the ul.todo-items
   todoList.appendChild(newTodoItem);
+}
+
+function addTodo(myeventi) {
+  // this EVENTI should prevent the form from submitting
+  myeventi.preventDefault();
+  //Show todo in dom
+  showTodoInDom(inputBox.value);
   // Save To Storage
   saveToLocalStorage(inputBox.value);
   // Claring the input after taking it's value
@@ -54,25 +73,59 @@ function saveOrDelete(ev) {
     pressedItemParent.addEventListener(
       "transitionend", function () {
         pressedItemParent.remove();
-        // removeFromLocalStorage();
-      })
+    })
+  const todoTextContent = pressedItemParent.children[0].innerText;
+  deleteFromLocalStorage(todoTextContent);
   } else if (pressedItem.classList.contains("done-btn")) {
     // if done-btn is pressed
     pressedItemParent.classList.add("mark-as-done");
   }
 }
 
+function deleteFromLocalStorage(todoTextContentToDelete) {
+  const todoItems = JSON.parse(localStorage.getItem("todoItems"));
+  todoItems.splice(todoItems.indexOf(todoTextContentToDelete), 1);
+  localStorage.setItem("todoItems", JSON.stringify(todoItems));
+}
+
 function saveToLocalStorage(newTodoItems) {
   let todoItems;
   // get items if they exist in storage already
   if (localStorage.getItem("todoItems") === null) {
-    // initialize an empty array
     todoItems = [];
-  } else {// parse in the data.
+  } else {
     todoItems = JSON.parse(localStorage.getItem("todoItems"));
   }
   // add the new todo to the list
   todoItems.push(newTodoItems);
-  // Save to localstorage
   localStorage.setItem("todoItems", JSON.stringify(todoItems));
 }
+
+console.log(todoList.childNodes);
+// /*
+function filterfunc(ev) {
+  // grab all todos
+  const todoItems = todoList.childNodes;
+  todoItems.forEach(function (eachTodo) {
+  switch (ev.target.value) {
+    case "all":
+      eachTodo.style.display = "flex";
+    break;
+    case "completed":
+      if (eachTodo.classList.contains("mark-as-done")) {
+        eachTodo.style.display = "flex";
+      } else {
+        eachTodo.style.display = "none";
+      }
+      break;
+    case "uncompleted":
+      if (!eachTodo.classList.contains("mark-as-done")) {
+        eachTodo.style.display = "flex";
+      } else {
+        eachTodo.style.display = "none";
+      }
+      break;
+  }
+  });
+}
+// */
